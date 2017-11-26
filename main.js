c757f467d0c47d3e79d39ffcfb90acbbba9aeac4
Nicolas <nkao552@gmail.com>
2017-11-26 13:29:34 -0800

    first commit'

---
+++ main.js
@@ -0,0 +1,19 @@
+window.addEventListener('keydown', (e) =>{
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+	if (!audio){console.log("No Audio"); return};
+	audio.currentTime = 0; // resets the audio so you can press the key repeatedly
+	audio.play();
+
+	key.classList.add('playing');
+
+});
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+function removeTransition(e) {
+	if(e.propertyName !== 'transform'){return};
+	this.classList.remove('playing');
+}
